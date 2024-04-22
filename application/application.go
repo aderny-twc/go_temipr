0b924a7fbf28eba830863e68b86184b2bac7d3cd
Andrey Belskiy <anb@smena.space>
2024-04-22 18:46:15 +0500

    Add graceful shutdown

--- application/application.go
+++ application/application.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -37,9 +38,23 @@ func (a *App) Start(ctx context.Context) error {
 
 	fmt.Println("Starting server")
 
-	err = server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("failed to start server %w", err)
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server %w", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		return server.Shutdown(timeout)
 	}
 
 	return nil
--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/aderny-twc/go_temipr/application"
 )
 
 func main() {
 	app := application.New()
+
+	signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 
 	err := app.Start(context.TODO())
 	if err != nil {

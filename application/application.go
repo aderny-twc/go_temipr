c14cadc1632f769e5fdd2ddc0976d94c9806ead6
Andrey Belskiy <anb@smena.space>
2024-04-22 18:26:20 +0500

    Add redis and code for checking connection to it

--- application/application.go
+++ application/application.go
@@ -4,15 +4,19 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	application := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 
 	return application
@@ -25,7 +29,15 @@ func (a *App) Start(ctx context.Context) error {
 		Handler: a.router,
 	}
 
-	err := server.ListenAndServe()
+	err := a.rdb.Ping(ctx).Err()
+	if err != nil {
+		return fmt.Errorf("failed to connect to redis %w", err)
+
+	}
+
+	fmt.Println("Starting server")
+
+	err = server.ListenAndServe()
 	if err != nil {
 		return fmt.Errorf("failed to start server %w", err)
 	}
